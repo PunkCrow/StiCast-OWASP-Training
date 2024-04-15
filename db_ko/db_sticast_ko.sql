a77f490c9ab5a3bed19ba670ccfbc904b4602c66
PunkCrow <nik7.box@proton.me>
2024-04-15 18:32:22 +0200

    First commit

---
+++ db_ko/db_sticast_ko.sql
@@ -0,0 +1,267 @@
+CREATE DATABASE  IF NOT EXISTS `db_sticast_ko` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `db_sticast_ko`;
+-- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_sticast2
+-- ------------------------------------------------------
+-- Server version	8.0.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `balance` double NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (1,'admin','admin123','admin','admin@admin.it',100,1),(2,'pippo','pippo123','Pippo','pippo@pippo.it',100,0);
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Cryptocurrency'),(2,'Sport'),(3,'Politics'),(4,'Economy');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `text` varchar(500) NOT NULL,
+  `timestamp` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Commenti_Domanda1_idx` (`question_id`),
+  KEY `fk_Commenti_Account1_idx` (`account_id`),
+  CONSTRAINT `fk_Commenti_Account1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
+  CONSTRAINT `fk_Commenti_Domanda1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `follow`
+--
+
+DROP TABLE IF EXISTS `follow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `follow` (
+  `account_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  KEY `fk_following_account1_idx` (`account_id`),
+  KEY `fk_following_question1_idx` (`question_id`),
+  CONSTRAINT `fk_following_account1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
+  CONSTRAINT `fk_following_question1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `follow`
+--
+
+LOCK TABLES `follow` WRITE;
+/*!40000 ALTER TABLE `follow` DISABLE KEYS */;
+/*!40000 ALTER TABLE `follow` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forecast`
+--
+
+DROP TABLE IF EXISTS `forecast`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `forecast` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `answer` varchar(3) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Forecast_Domanda1_idx` (`question_id`),
+  KEY `fk_Forecast_Account1_idx` (`account_id`),
+  CONSTRAINT `fk_Forecast_Account1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
+  CONSTRAINT `fk_Forecast_Domanda1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forecast`
+--
+
+LOCK TABLES `forecast` WRITE;
+/*!40000 ALTER TABLE `forecast` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forecast` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `forecast_AFTER_INSERT` AFTER INSERT ON `forecast` FOR EACH ROW BEGIN
+
+IF EXISTS (SELECT * FROM share WHERE question_id=NEW.question_id AND account_id = NEW.account_id) THEN 
+	IF (new.answer = 'yes') THEN
+		UPDATE share SET yesShareQuantity = yesShareQuantity + NEW.quantity WHERE question_id=NEW.question_id AND account_id = NEW.account_id; 
+	ELSE 
+		UPDATE share SET noShareQuantity = noShareQuantity + NEW.quantity WHERE question_id=NEW.question_id AND account_id = NEW.account_id; 
+	END IF;
+ELSE
+IF (new.answer = 'yes') THEN
+		INSERT INTO share (question_id, account_id, yesShareQuantity, noShareQuantity) VALUES (new.question_id, new.account_id, new.quantity, 0); 
+	ELSE 
+		INSERT INTO share (question_id, account_id, yesShareQuantity, noShareQuantity) VALUES (new.question_id, new.account_id, 0, new.quantity); 
+	END IF;
+
+END IF;
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `text` varchar(300) NOT NULL,
+  `yesValue` double NOT NULL DEFAULT '0.5',
+  `noValue` double NOT NULL DEFAULT '0.5',
+  `yesShare` int(11) NOT NULL DEFAULT '0',
+  `noShare` int(11) NOT NULL DEFAULT '0',
+  `isOpen` tinyint(4) NOT NULL,
+  `forecastCount` int(11) NOT NULL,
+  `creationDate` varchar(45) NOT NULL,
+  `expirationDate` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Domanda_Categoria1_idx` (`category_id`),
+  CONSTRAINT `fk_Domanda_Categoria1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,1,'Will bitcoin succeed in exceeding the 10k$ threshold by 2020?',0.5,0.5,0,0,1,0,'01/01/2020','31/12/2020'),(2,2,'Will SSC Napoli win Serie A 2020/21?',0.5,0.5,0,0,1,0,'15/08/20','01/05/2021'),(3,2,'Will LeClerc win the F1 World Championship 2020?',0.5,0.5,0,0,1,0,'01/01/2020','31/12/2020'),(4,1,'Will Ethereum be able to return to $1000 by December?',0.5,0.5,0,0,1,0,'01/01/2020','31/12/2020'),(5,4,'Italy\'s GDP will grow in the third quarter?',0.5,0.5,0,0,1,0,'01/05/2020','31/12/2020'),(6,3,'Will Trump succeed in building the wall?',0.5,0.5,0,0,0,0,'01/05/2018','31/12/2018'),(7,3,'Will Trump be re-elected in 2020?',0.5,0.5,0,0,1,0,'01/01/2020','1/11/2020');
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `share`
+--
+
+DROP TABLE IF EXISTS `share`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `share` (
+  `question_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `yesShareQuantity` int(11) NOT NULL,
+  `noShareQuantity` int(11) NOT NULL,
+  PRIMARY KEY (`question_id`,`account_id`),
+  KEY `fk_share_Domanda1_idx` (`question_id`),
+  KEY `fk_share_Account1_idx` (`account_id`),
+  CONSTRAINT `fk_share_Account1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
+  CONSTRAINT `fk_share_Domanda1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `share`
+--
+
+LOCK TABLES `share` WRITE;
+/*!40000 ALTER TABLE `share` DISABLE KEYS */;
+/*!40000 ALTER TABLE `share` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-03 17:26:31
